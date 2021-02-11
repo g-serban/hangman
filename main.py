9740d036179cd20c9d4febd56a73e1a9f280ddd8
gserban <gserban1233@gmail.com>
2021-02-11 19:42:22 +0200

    finishing project

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import string
 
 def get_valid_words(words):
     selected_word = random.choice(words)
-
-    while '-' in selected_word or ' ':   # because we have some nouns with - and spaces, we need to iterate to the next word 
+    while '-' in selected_word or ' ' in selected_word:   # because we have some nouns with - and spaces, we need to iterate to the next word 
         selected_word = random.choice(words)
 
     return selected_word.upper()
@@ -14,15 +13,40 @@ def get_valid_words(words):
 
 def hangman():
     word = get_valid_words(list_of_words)
-    word_letter = set(word)    # here we track the letters in the word
+    word_letters = set(word)    # here we track the letters in the word
     alphabet = set(string.ascii_uppercase)
     letters = set()   # here we track what uppercase letters the user has guessed 
-    user_input = set(input('Guess a letter: '))   
-    if user_input in alphabet - letters:      # if the input is in the alphabet, then add it to used_letters set
-        letters.add(user_input)
-        if user_input in word_letter:
-            word_letter.remove(user_input)
-    elif
+    lives = 6
+
+    # getting user input
+    while len(word_letters) > 0 and lives >= 1:
+        # letters used
+        print('You have', lives, 'left and you have used these letters: ', ' '.join(letters))
+
+        # what the current word is but with dashes for where the user hasn't guessed yet
+        word_list = [letter if letter in letters else '-' for letter in word]
+        print('Current word is: ', ' '.join(word_list))
+
+        user_input = input('Guess a letter: ').upper()
+        if user_input in alphabet - letters:      # if the input is in the alphabet, then add it to used_letters set
+            letters.add(user_input)
+            if user_input in word_letters:
+                word_letters.remove(user_input)
+            else:
+                lives = lives - 1  # takes away a life if wrong
+        elif user_input in letters:
+            print('You already guessed that letter before!')
+            continue
+        else:
+            print('Invalid character. Please try again!')
+            continue
+    # gets here when 1. user guesses all the words or 2. no lives left
+    if lives == 0:
+        print('You have died!! The word was', word)
+    else:
+        print('You guessed the', word, 'correctly. Congrats!!')
+        
+hangman()
 
 
 
